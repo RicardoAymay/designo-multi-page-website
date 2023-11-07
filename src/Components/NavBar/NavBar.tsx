7cbfdbc4bf5d2aefe1e4efe9143ad5a81b753aaa
RicardoAymay <r.aymay.goncalves@gmail.com>
2023-11-07 10:49:23 -0300

    feat: navbar desktop added. Suggestions for global and modifications added

--- src/Components/NavBar/NavBar.tsx
+++ src/Components/NavBar/NavBar.tsx
@@ -4,7 +4,7 @@ import "./navBar.sass"
 const NavBar = () => {
   return (
     <div className="navContainer">
-        <Link to={"/"}><img src={logo} alt="Logo from DESIGNO!"/></Link>
+       <Link to={"/"} className="navLogo"> <span className="navDesigno">DESIGNO</span><img src={logo} alt="Logo from DESIGNO!"/></Link>
         <nav>
             <Link className="navlink" to={"about"}> OUR COMPANY </Link>
             <Link className="navlink" to={"location"}> LOCATIONS </Link>
