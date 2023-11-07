4c644adfbbb0d91178365229b1cf76229bc71274
RicardoAymay <r.aymay.goncalves@gmail.com>
2023-11-07 12:21:57 -0300

    chore: started implementing the presentation card

--- src/Components/PresentationCard/PresentationCard.tsx
+++ src/Components/PresentationCard/PresentationCard.tsx
@@ -1,16 +1,24 @@
-import cellphone from "../../assets/home/desktop/image-hero-phone.png"
-import circle from "../../assets/home/desktop/bg-pattern-hero-home.svg"
+import cellphone from "../../assets/home/desktop/image-hero-phone.png";
+import circle from "../../assets/home/desktop/bg-pattern-hero-home.svg";
+import "./presentationCard.sass";
+
 const PresentationCard = () => {
   return (
-    <section>
-      <h3>Award-winning custom designs and digital branding solutions</h3>
-      <p>
-        With over 10 years in the industry, we are experienced in creating fully
-        responsive websites, app design, and engaging brand experiences. Find
-        out more about our services.
-      </p>
-      <img src={cellphone} alt="This is technology to see the art!"/>
-      <img src={circle} alt=""/>
+    <section className="presentationCardContainer">
+      <div className="textSide">
+        <h3>Award-winning custom designs and digital branding solutions</h3>
+        <p>
+          With over 10 years in the industry, we are experienced in creating
+          fully responsive websites, app design, and engaging brand experiences.
+          Find out more about our services.
+        </p>
+
+        <button>Learn More</button>
+      </div>
+      <figure className="imgsSide">
+        <img className="cellAndcircle" id="presentationCell" src={cellphone} alt="This is technology to see the art!" />
+        <img className="cellAndcircle" id="presentationCircle" src={circle} alt="" />
+      </figure>
     </section>
   );
 };
