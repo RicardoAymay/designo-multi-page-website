da2d76cb3e3233962c45015498988682dc6ba00a
RicardoAymay <r.aymay.goncalves@gmail.com>
2023-11-10 16:25:34 -0300

    chore: work in progress

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,13 +1,16 @@
 import NavBar from "../Components/NavBar/NavBar"
 import PresentationCard from '../Components/PresentationCard/PresentationCard';
 
-// import {c}
+
 const Home = () => {
   return (
-//  container
+
     <div className="container"> 
     <NavBar />
+    
+
     <PresentationCard/>
+    
     
     
   </div> 
